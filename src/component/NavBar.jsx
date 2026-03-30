10dd6f66cfd3ed8a08fd3efa564dfd6a29f35de5
Iqbal <iqbalhossain6615@gmail.com>
2026-03-30 15:44:43 +0600

    navbar done now

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+
+const NavBar = () => {
+    return (
+        <div className="navbar border border-[#F2F2F2]">
+            <div className="max-w-7xl mx-auto w-full flex justify-between items-center">
+
+                <div className="navbar-start">
+                    <h2 className="font-bold text-3xl bg-gradient-to-r from-blue-800 to-purple-600 bg-clip-text text-transparent">DigiTools</h2>
+                </div>
+
+                <div className="navbar-center hidden md:flex">
+                    <ul className="menu menu-horizontal gap-8 px-1 text-lg">
+                        <li><a>Products</a></li>
+                        <li><a>Features</a></li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Testimonials</a></li>
+                        <li><a>FAQ</a></li>
+                    </ul>
+                </div>
+
+                <div className="navbar-end gap-2">
+                    <button className="p-2 rounded-full hover:bg-gray-100">
+                        <FiShoppingCart className="text-xl" />
+                    </button>
+                    <ul className="menu menu-horizontal text-lg"><li><a href="">Login</a></li></ul>
+                    <button className="btn bg-gradient-to-r from-blue-800 to-purple-600 rounded-full text-white">Get Started</button>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
